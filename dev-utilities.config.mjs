1fca8c6a11976cfaf19a31b1a40ae6bf8fa014bf
Massimo Cassandro <massimo.cassandro@gmail.com>
2024-08-07 22:14:16 +0200

    v.0.9.2 - upd

--- dev-utilities.config.mjs
+++ dev-utilities.config.mjs
@@ -11,24 +11,7 @@ const config = {
     defaultDescr     : 'Fix'    // default null
   },
 
-  /*
-    Configurazione per `vscode-css-properties-list`
-  */
-  // cssPropsList: {
 
-  //   sources: [
-  //     './test/vscode-css-custom-properties-list/test.css'
-  //   ],
-  //   snippet_file           : './test/vscode-css-custom-properties-list/myproject.code-snippets',
-  //   result_test_file       : './test/vscode-css-custom-properties-list/css-debug-file.css',
-  //   snippet_key            : 'custom props list',
-  //   custom_var_prefix      : ['prefix-'],
-  //   icon_sources: [
-  //     './test/vscode-css-custom-properties-list/icon-list.js'
-  //   ],
-  //   icon_list_snippet_key  : 'icons list'
-
-  // }
 };
 
 export default config;
---
+++ stylelint.config.cjs
@@ -0,0 +1,13 @@
+/* eslint-env node */
+
+module.exports = {
+ extends: ['@massimo-cassandro/stylelint-config'],
+ ignoreFiles: []
+
+ // tailwind
+ // 'rules': {
+ // 'value-keyword-case': null,
+ // '@stylistic/number-no-trailing-zeros': null
+ // }
+};
+
